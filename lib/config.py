884a574fae780eef317167fd684aea8f2c6efc0b
Ciaran <>
2023-02-18 15:08:59 +0000

    Cache config file.
    Use a global to cache the config file so every time a module accesses the config we are not
    doing a file read of something that hasn't changed.

--- lib/config.py
+++ lib/config.py
@@ -1,17 +1,21 @@
+import logging
 import toml
 from pathlib import Path
 
+toml_config = None
+logger = logging.getLogger("wordpress-plugin-grep")
+
 
 def get():
-    toml_config = None
-
     def __read_config():
         with open(Path("config/settings.toml")) as config_file_handle:
+            logger.debug("Opened config file. %s", config_file_handle)
             toml_config_ = toml.load(config_file_handle)
         return toml_config_
 
-    if toml_config:
-        return toml_config
-    else:
+    global toml_config
+
+    if toml_config is None:
         toml_config = __read_config()
-        return toml_config
+
+    return toml_config
