0c2ee5bb7f479e9eac07193ed4f3bb0448c551c4
Ciaran <>
2023-03-17 21:08:41 +0000

    Check each regular expression iteratively.
    If the regular expression pattern is given as one big string, it makes it
    very difficult if not impossible to find out for a specific match which regular expression pattern
    caused the match.
    
    To get around this I have compiled the regular expression as a list of compiled regular expression patterns.
    
    This way I can map which regular expression pattern triggered the match.
    
    For example.
    
    Previously the regular expression would have been a single compiled regular expression pattern like
    
    pattern = '\$wpdb\-\>prepare|nopriv|add_action'

--- lib/find_functionality.py
+++ lib/find_functionality.py
@@ -8,41 +8,39 @@ from lib.paths import plugin_extraction_path
 logger = logging.getLogger(f"wordpress-plugin-grep.{__name__}")
 
 
-def __build_plugin_feature_mapping_dict(plugin_names, compiled_regular_expression):
+def __build_plugin_feature_mapping_dict(plugin_names, regular_expression_patterns):
     plugin_features = dict()
     for plugin_name in plugin_names:
         plugin_features[plugin_name] = dict()
-        for re_pattern in compiled_regular_expression.pattern.split('|'):
-            feature = re_pattern.replace('\\', '')
-            plugin_features[plugin_name][feature.lower()] = 0
+        for re_pattern in regular_expression_patterns:
+            plugin_features[plugin_name][re_pattern.pattern.lower()] = 0
     return plugin_features
 
 
 def check_plugins():
     config = get_config()
-    regular_expression_patterns = '|'.join(config.get('checks').get('regular_expression_patterns'))
-    valid = re.compile(regular_expression_patterns, flags=re.MULTILINE | re.IGNORECASE)
-
+    regular_expression_patterns = [re.compile(re.escape(pattern), flags=re.MULTILINE | re.IGNORECASE) for pattern in config.get('checks').get('regular_expression_patterns')]
     extraction_path = plugin_extraction_path()
 
     plugin_names = [plugin_name.stem for plugin_name in extraction_path.glob("*")]
 
-    plugin_features = __build_plugin_feature_mapping_dict(plugin_names, valid)
+    plugin_features = __build_plugin_feature_mapping_dict(plugin_names, regular_expression_patterns)
 
     for plugin in plugin_names:
         plugin_dir = Path(extraction_path, plugin)
-        for php_file in plugin_dir.glob("**/**/*.php"):
+        for php_file in plugin_dir.glob("**/*.php"):
             if php_file.is_dir():
                 continue
             with open(php_file) as php_file_handle:
                 try:
                     data = php_file_handle.read()
-                    match = valid.search(data)
-                    if match:
-                        # case-insensitive keys for the dictionary
-                        plugin_features[plugin][match.group().lower()] += 1
+                    for re_pattern in regular_expression_patterns:
+                        match = re_pattern.search(data)
+                        if match:
+                            # case-insensitive keys for the dictionary
+                            plugin_features[plugin][re_pattern.pattern.lower()] += 1
                 except UnicodeDecodeError:
-                    logger.warn("Failed to decode unicode symbol when reading file %s", php_file)
+                    logger.warning("Failed to decode unicode symbol when reading file %s", php_file)
 
     def sort_by_inner_dictionary_value(plugin_name_to_features, sort_key_):
         # The second element of each tuple is a dictionary containing the tally of how many times
--- tests/test_tests_work.py
+++
@@ -1,5 +0,0 @@
-import pytest
-
-@pytest.xfail('pytest is working.')
-def test_something(self):
-    self.assertEqual(True, False)
