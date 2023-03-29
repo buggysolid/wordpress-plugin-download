d1b326ef10e2f7bb7683f6784493e599a9cde17e
Ciaran <>
2023-03-29 16:23:18 +0100

    Remove find functionality feature.
    The find feature was fun to write but it was not implemented well.
    
    It was essentially a find command piped into grep with regular expressions but handling less
    edge cases.
    
    The main issue I had when I first wrote this code was that it was difficult to download and
    extract a large amount of wordpress plugins matching a certain criteria to a single directory.
    
    I have pushed the code that has the find and report generating feature to the 'find' branch.
    
    There are much better SAST tools such as SonarQube and Semgrep.
    
    https://semgrep.dev/
    https://www.sonarsource.com/products/sonarqube/
    
    I'd recommend using this Python CLI to collect the plugins you need and then do the actually code analysis with the above tools.

--- run.py
+++ run.py
@@ -3,8 +3,6 @@ import logging
 
 from lib.download_plugins import get_plugins
 from lib.extract_plugins import extract_plugins
-from lib.find_functionality import check_plugins
-from lib.report import write_report
 
 argparse = argparse.ArgumentParser("wordpress-plugin-grep", "python run.py", "A program to use the "
                                                                              "WordPress plugin API to"
@@ -15,7 +13,6 @@ group = argparse.add_mutually_exclusive_group()
 
 group.add_argument("-d", "--download-only", action="store_true")
 group.add_argument("-e", "--extract-existing", action="store_true")
-group.add_argument("-c", "--check-existing", action="store_true")
 group.add_argument("-f", "--full-run", action="store_true")
 
 arguments = argparse.parse_args()
@@ -40,18 +37,8 @@ if arguments.download_only:
 elif arguments.extract_existing:
     logger.info("Extracting already downloaded plugins.")
     extract_plugins()
-elif arguments.check_existing:
-    logger.info("Searching extracted plugins for features.")
-    extract_plugins()
-    features = check_plugins()
-    logger.info("Writing report to report.txt.")
-    write_report(features)
 elif arguments.full_run:
     logger.info("Downloading plugins.")
     get_plugins()
     logger.info("Extracting plugins.")
     extract_plugins()
-    logger.info("Searching for features in plugins.")
-    features = check_plugins()
-    logger.info("Writing report to report.txt.")
-    write_report(features)
